1fb1f2446ed15a85eedf0fb5de2dfeb60476d0f2
manikandanj8 <87042412+manikandanj8@users.noreply.github.com>
2021-08-03 00:00:53 +0530

    Update Test_AccountsOverview.java

--- com.parasoft.parabank_POM/src/testcases/Test_AccountsOverview.java
+++ com.parasoft.parabank_POM/src/testcases/Test_AccountsOverview.java
@@ -1,6 +1,6 @@
 package testcases;
 
-import org.apache.log4j.Logger;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import commonfile.CommonFunctions;
@@ -9,12 +9,12 @@ import pageobjects.PO_login;
 
 public class Test_AccountsOverview extends CommonFunctions {
 
-	static Logger logger = Logger.getLogger(Test_AccountsOverview.class);
+	
 
 	public void login() {
 
 		PageFactory.initElements(driver, PO_login.class);
-		/*logger.info("logging into the application");*/
+
 		PO_login.username.sendKeys(properties.getProperty("username"));
 		PO_login.password.sendKeys(properties.getProperty("password"));
 		PO_login.button.click();
@@ -25,12 +25,12 @@ public class Test_AccountsOverview extends CommonFunctions {
 	public void accountsoverview() {
 
 		login();
-		/*logger.info("1st - Account Over view Start");*/
+		
 		PageFactory.initElements(driver, PO_AccountsOverview.class);
 		PO_AccountsOverview.accoverview.click();
 		String bal = PO_AccountsOverview.balance.getText();
 		System.out.println("There is an " + bal);
-		/*logger.info("1st - Account Over view End");*/
+		
 
 	}
 }
