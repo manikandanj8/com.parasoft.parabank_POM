4dbaf0c23b91513aee436ddac1ee4088670898ea
manikandanj8 <87042412+manikandanj8@users.noreply.github.com>
2021-08-03 00:02:20 +0530

    Update Test_login.java

--- com.parasoft.parabank_POM/src/testcases/Test_login.java
+++ com.parasoft.parabank_POM/src/testcases/Test_login.java
@@ -1,33 +1,26 @@
 package testcases;
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import commonfile.CommonFunctions;
 import pageobjects.PO_login;
 
-@SuppressWarnings("unused")
+
 public class Test_login extends CommonFunctions {
 
-	Logger logger = Logger.getLogger(Test_login.class);
+	
 
 	@Test
 	public void login() {
 
-	/*	logger.info("Logging into the Site");*/
+	
 		PageFactory.initElements(driver, PO_login.class);
 		PO_login.username.sendKeys(properties.getProperty("username"));
 		PO_login.password.sendKeys(properties.getProperty("password"));
 		PO_login.button.click();
 		System.out.println(PO_login.welcome.getText());
-		/*logger.info("Logged in Successfully");*/
+
 
 		/* PO_login po_login = new PO_login(); */
 		/*
